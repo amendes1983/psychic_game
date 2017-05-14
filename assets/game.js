f16e72995682c770699cdb7c3d0df278ce504902
Aaron Mendes <Aaron Mendes>
2017-05-14 15:30:43 -0700

    added html button

--- assets/game.js
+++ assets/game.js
@@ -1,6 +1,4 @@
-
-
-    // variable letters
+// variable letters
 var letters = ['a', 'b', 'c',  'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
@@ -19,22 +17,25 @@ console.log(psykerChoice);
 
 //Player presses key and funnction will run...
 document.onkeyup = function(event) {
-  var userGuess = event.key;
-  var guessedLetters = event.key;
-  if(userGuess === psykerChoice){
-      wins++;
-      guesses =10;
-  }else{
-      guesses--;
-  };
-
-  if(guesses === 0){
-      losses++;
-      guesses = 10;
-  };
-
-  document.getElementById('wins').innerHTML = "Wins: " + wins;
-  document.getElementById('losses').innerHTML = "Losses: " + losses;
-  document.getElementById('guesses').innerHTML = "Guesses Left: " + guesses;
-  document.getElementById('user_guesses').innerHTML = "Letter you guessed: " + guessedLetters;
+var userGuess = event.key;
+var guessedLetters = event.key;
+if(userGuess === psykerChoice){
+  wins++;
+  guesses = 10;
+}
+else{
+  guesses--;
 };
+
+if(guesses === 0){
+  losses++;
+  guesses = 10;
+};
+
+
+document.getElementById('wins').innerHTML = "Wins: " + wins;
+document.getElementById('losses').innerHTML = "Losses: " + losses;
+document.getElementById('guesses').innerHTML = "Guesses Left: " + guesses;
+document.getElementById('user_guesses').innerHTML = "Letter you guessed: " + guessedLetters;
+};
+//Clicking button will pick a new letter ?????? :(...
